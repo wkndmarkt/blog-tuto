c3bee9ef1c59e174249cc094ee1a6e9817ed333d
wkndmarkt <difang@free.fr>
2017-02-20 12:05:33 +0100

    ajout blog détaillé et css

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.utils.text import Truncator
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display   = ('title','author','created_date','apercu_text')
+    list_filter    = ('author',)
+    ordering       = ('created_date',)
+    search_fields  = ('title','text')
+    
+    def apercu_text(self, text):
+        return Truncator(text).chars(40, truncate='…')
+
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all().order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
